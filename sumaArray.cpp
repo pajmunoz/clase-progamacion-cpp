cba8fe96bc13361f923f5521d371d8642845d359
Pablo Jara <pablojaramunoz@icloud.com>
2025-10-03 22:00:14 -0500

    gitignore fix

---
+++ sumaArray.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    int suma = 0;
+    int a[5] = {1, 2, 3, 4, 5};
+    for (int i = 0; i < 5; i++){
+        suma += a[i];
+    }
+    cout << "La suma es: " << suma << endl;
+    return 0;       
+}
