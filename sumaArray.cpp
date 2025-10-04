44398b5b36a9ae99f2e83a317dc646b141623582
Pablo Jara <pablojaramunoz@icloud.com>
2025-10-03 22:18:12 -0500

    sumaarray

--- sumaArray.cpp
+++ sumaArray.cpp
@@ -5,6 +5,7 @@ int main (){
     int suma = 0;
     int a[5] = {1, 2, 3, 4, 5};
     for (int i = 0; i < 5; i++){
+        cout << "Numero " << i+1 << ": " << a[i] << endl;
         suma += a[i];
     }
     cout << "La suma es: " << suma << endl;
